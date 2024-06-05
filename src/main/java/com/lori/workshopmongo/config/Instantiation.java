2e148c60c4ea927d70e1dc0ece5e8991897ca3ab
Lori Medeiros <cecilialmedeirosc@gmail.com>
2024-06-05 20:05:13 -0300

    Operação de instanciação da base de dados

---
+++ src/main/java/com/lori/workshopmongo/config/Instantiation.java
@@ -0,0 +1,30 @@
+package com.lori.workshopmongo.config;
+
+import com.lori.workshopmongo.domain.User;
+import com.lori.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        userRepository.deleteAll(); //isso aqui é necessário pois, como brinquei antes com o mongodb, já tem dados lá
+
+        User lori = new User(null, "Lori Medeiros", "lori@leromail.com");
+        User yves = new User(null, "Yves Pereira", "yves@leromail.com");
+        User pedro = new User(null, "Pedro Henrique", "pedenrique@leromail.com");
+
+        userRepository.saveAll(Arrays.asList(lori, yves, pedro));
+
+    }
+
+}
